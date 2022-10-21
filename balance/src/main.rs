e158e16be8e92938ad57f12f042e343723915493
Francisco Aguirre <franciscoaguirreperez@gmail.com>
2022-10-21 16:47:01 -0300

    Finish balance microservice

--- balance/src/main.rs
+++ balance/src/main.rs
@@ -1,53 +1,119 @@
 #[macro_use]
 extern crate rocket;
 
-use std::fmt::Write;
-
 use futures::stream::StreamExt;
-use rocket_db_pools::mongodb;
-use rocket_db_pools::mongodb::bson::{doc, Document};
+use rocket::serde::{json::Json, Deserialize, Serialize};
+use rocket_db_pools::mongodb::{
+    self,
+    bson::{self, doc},
+};
 use rocket_db_pools::{Connection, Database};
 
+const DEFAULT_BALANCE_AMOUNT: f64 = 1000.0;
+
 #[derive(Database)]
-#[database("balance")]
+#[database("main")]
 struct MongoDB(mongodb::Client);
 
-#[post("/insert")]
-async fn insert(db: Connection<MongoDB>) {
-    let collection = db.database("balances").collection::<Document>("books");
+#[get("/databases")]
+async fn list_databases(db: Connection<MongoDB>) -> String {
+    let databases = db.list_database_names(None, None).await.unwrap();
 
-    let docs = vec![
-        doc! { "title": "1984", "author": "George Orwell" },
-        doc! { "title": "Animal Farm", "author": "George Orwell" },
-        doc! { "title": "The Great Gatsby", "author": "F. Scott Fitzgerald" },
-    ];
-
-    collection.insert_many(docs, None).await.unwrap();
+    format!("{:?}", databases)
 }
 
-#[get("/list")]
-async fn list(db: Connection<MongoDB>) -> String {
-    let collection = db.database("balance").collection::<Document>("books");
+#[derive(Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct MoneyMovement<'r> {
+    customer_id: &'r str,
+    amount: f64,
+}
 
-    let mut return_value = String::new();
-    let mut cursor = collection.find(None, None).await.unwrap();
-    while let Some(result) = cursor.next().await {
-        if let Ok(document) = result {
-            write!(&mut return_value, "{}", document).unwrap();
+#[derive(Serialize)]
+#[serde(crate = "rocket::serde")]
+struct MoneyMovementResponse {
+    amount: f64,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(crate = "rocket::serde")]
+struct Balance {
+    customer_id: String,
+    amount: f64,
+}
+
+#[post("/register", data = "<money_movement>")]
+async fn register(
+    money_movement: Json<MoneyMovement<'_>>,
+    db: Connection<MongoDB>,
+) -> Json<MoneyMovementResponse> {
+    let balances = db.database("main").collection::<Balance>("balances");
+    let customer_balance = balances
+        .find_one(doc! { "customer_id": money_movement.customer_id }, None)
+        .await
+        .expect("Could not fetch customer balance");
+
+    let new_balance = match customer_balance {
+        None => {
+            let new_amount = DEFAULT_BALANCE_AMOUNT + money_movement.amount;
+
+            if new_amount < 0.0 {
+                panic!("Not enough money!");
+            }
+
+            let new_balance = Balance {
+                customer_id: money_movement.customer_id.to_owned(),
+                amount: new_amount,
+            };
+
+            balances.insert_one(new_balance, None).await.unwrap();
+
+            Json(MoneyMovementResponse { amount: new_amount })
         }
-    }
+        Some(old_balance) => {
+            let amount = old_balance.amount;
+            let new_amount = amount + money_movement.amount;
 
-    return_value
+            if new_amount < 0.0 {
+                panic!("Not enough money!");
+            }
+
+            let bson = bson::to_bson(&old_balance).unwrap();
+            let document = bson.as_document().unwrap();
+
+            balances
+                .update_one(
+                    document.to_owned(),
+                    doc! { "$set": { "amount": new_amount } },
+                    None,
+                )
+                .await
+                .unwrap();
+
+            Json(MoneyMovementResponse { amount: new_amount })
+        }
+    };
+
+    new_balance
 }
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+#[get("/balances")]
+async fn list_balances(db: Connection<MongoDB>) -> String {
+    let balances_collection = db.database("main").collection::<Balance>("balances");
+
+    let balances: Vec<_> = balances_collection
+        .find(None, None)
+        .await
+        .unwrap()
+        .collect()
+        .await;
+
+    format!("{:?}", balances)
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .attach(MongoDB::init())
-        .mount("/", routes![index, insert, list])
+        .mount("/", routes![list_databases, register, list_balances])
 }
